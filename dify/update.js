f2580f2e961d953e1125e5d5255fe7cc8c916c9c
Ahson Shaikh <ahsonshaikh616@gmail.com>
2025-05-24 00:55:10 +0500

    Testing After Removing Ports

--- dify/update.js
+++ dify/update.js
@@ -4,3 +4,10 @@ await utils.cloneOrPullRepo({ repo: "https://github.com/langgenius/dify.git" });
 await utils.copyDir("./repo/docker", "./code");
 await utils.removeContainerNames("./code/docker-compose.yaml");
 await utils.removePorts("./code/docker-compose.yaml");
+
+
+await utils.searchReplace(
+    "./code/.env.example",
+    "APP_WEB_URL=",
+    "APP_WEB_URL=https://$(PRIMARY_DOMAIN)"
+  );
